b7a70eb74226b1b2e7f54846d03db47018e9769c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-03-12 23:56:55 +0000

    vendor: Add FaceUnlock
    Signed-off-by: anupam41 <anupamsamanta41@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -68,7 +68,7 @@ PRODUCT_COPY_FILES += \
 
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
@@ -164,5 +164,16 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/milk/build/target/product/security/milk
+
+#Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 
 include vendor/milk/config/version.mk
