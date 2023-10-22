ProjectBlaze/vendor_blaze

vendor: soong: Add camera_needs_client_info
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
Change-Id: I60a9b2a137d70e7592b43dd2f9a05bbb6a2ef694
(cherry picked from commit 5fcb5d558342aca3cd83a61530cb375ad3349048)

config/BoardConfigSoong.mk
@@ -56,6 +56,7 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_nothing_camera \
     needs_camera_boottime \
     camera_needs_client_info_lib \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -89,6 +90,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

ProjectMilk-AOSP/vendor_milk

config/BoardConfigSoong.mk
@@ -57,6 +57,7 @@ SOONG_CONFIG_milkGlobalVars += \
     uses_nothing_camera \
     needs_camera_boottime \
     camera_needs_client_info_lib \
+    camera_needs_client_info
 
 SOONG_CONFIG_NAMESPACES += milkNvidiaVars
 SOONG_CONFIG_milkNvidiaVars += \
@@ -91,6 +92,7 @@ SOONG_CONFIG_milkGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_milkGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_milkGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_milkGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
+SOONG_CONFIG_milkGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0