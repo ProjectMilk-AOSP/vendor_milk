ProjectBlaze/vendor_blaze

vendor: soong: Add camera_needs_client_info_lib

config/BoardConfigSoong.mk
@@ -55,6 +55,7 @@ SOONG_CONFIG_blazeGlobalVars += \
     uses_oplus_camera \
     uses_nothing_camera \
     needs_camera_boottime \
+    camera_needs_client_info_lib \
 
 SOONG_CONFIG_NAMESPACES += blazeNvidiaVars
 SOONG_CONFIG_blazeNvidiaVars += \
@@ -87,6 +88,7 @@ SOONG_CONFIG_blazeGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBD
 SOONG_CONFIG_blazeGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_blazeGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_blazeGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0

ProjectMilk-AOSP/vendor_milk

config/BoardConfigSoong.mk
@@ -56,6 +56,7 @@ SOONG_CONFIG_milkGlobalVars += \
     uses_oplus_camera \
     uses_nothing_camera \
     needs_camera_boottime \
+    camera_needs_client_info_lib \
 
 SOONG_CONFIG_NAMESPACES += milkNvidiaVars
 SOONG_CONFIG_milkNvidiaVars += \
@@ -73,7 +74,6 @@ SOONG_CONFIG_milkQcomVars += \
 endif
 
 # Soong bool variables
-<<<<<<< HEAD
 SOONG_CONFIG_milkGlobalVars_camera_override_format_from_reserved := $(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED)
 SOONG_CONFIG_milkGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_milkGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
@@ -90,6 +90,7 @@ SOONG_CONFIG_milkGlobalVars_uses_legacy_fd_fbdev := $(TARGET_USES_LEGACY_FD_FBDE
 SOONG_CONFIG_milkGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_milkGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_milkGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
+SOONG_CONFIG_milkGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0